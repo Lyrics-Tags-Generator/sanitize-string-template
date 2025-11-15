86077af8d543dec9bbc1913b7dc1fa0e864a3bda
Nicholas <contactnicholas81@gmail.com>
2025-11-15 21:34:42 +0000

    Remove , commas if the string starts with one

--- sanitize.py
+++ sanitize.py
@@ -2,12 +2,15 @@ import re
 
 def sanitize_string_template():
     string_template = input("Paste: ")
+    if string_template.startswith(','):
+        string_template = string_template[1:].lstrip()
     if not re.search(r'(?s)(?=.*\$)(?=.*\{)(?=.*\})(?=.*,)', string_template):
         print("Please provide a valid string template format.")
         sanitize_string_template()
     else:
         new_string_template = re.sub(r"\$", "", string_template)
         print("")
+        print("Result:")
         print(new_string_template)
         return
 
